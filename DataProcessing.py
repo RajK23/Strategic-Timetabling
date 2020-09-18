f93efa02e0be6b31cf0062ff2f95b264fcb818a9
Kausta <kau23@live.com>
2020-09-18 17:02:20 +1000

    implemented bi objective models

--- DataProcessing.py
+++ DataProcessing.py
@@ -5,7 +5,7 @@ import operator
 from collections import defaultdict
 
 script_dir = os.path.dirname(__file__)
-relpath = 'CCT/ITC2007/comp17.xml'
+relpath = 'CCT/ITC2007/comp01.xml'
 absFilePath = os.path.join(script_dir, relpath)
 
 tree = ET.parse(absFilePath)
@@ -19,7 +19,7 @@ Courses = []
 P = []
 D = []
 Curricula = {}
-T = defaultdict(list);
+T = defaultdict(list)
 L  = {} 
 MND = {} 
 DEM = {}
@@ -31,7 +31,7 @@ for child in root:
     if child.tag == "descriptor":
         for gchild in child:
             if (gchild.tag == "periods_per_day"):
-                PPD = int(gchild.attrib["value"]) 
+                PPD = int(gchild.attrib["value"])
     
     for gchild in child:
         attribs = gchild.attrib
@@ -65,43 +65,60 @@ for child in root:
 
 
 P = range(1, Days * PPD + 1)
-D = [[(j+1) + i*(PPD-1) for i in range(PPD)] for j in range(Days)]
+D = [[(j+1) + i*(Days) for i in range(PPD)] for j in range(Days)]
 SGT = {s : [ss for ss in S if ss >= s] for s in S}
 CGT = {s : [c for c in Courses if DEM[c] >= s] for s in S}
 RGT = {s : [c for c in Rooms if CAP[c] >= s] for s in S}
 
-
 def QualityProblem() :
     Quality = Model()
     X = {(c, p) : Quality.addVar(vtype = GRB.BINARY) for c in Courses for p in P}
-    W = {(c) : Quality.addVar() for c in Courses}
+    #print(X)
+    #Minimum working days
     Z = {(c, d) : Quality.addVar(vtype = GRB.BINARY) for c in Courses for d in range(len(D))}
+    W = {(c) : Quality.addVar(vtype = GRB.INTEGER) for c in Courses}
     Q = {(cu, p) : Quality.addVar(vtype = GRB.BINARY) for cu in Curricula for p in P}
     V = {(cu, p) : Quality.addVar(vtype = GRB.BINARY) for cu in Curricula for p in P}
-    Quality.setObjective(quicksum(5 * W[c] for c in Courses) + quicksum(2 * V[cu,p] for cu in Curricula for p in P));
 
-    b = {(s, p) : Quality.addConstr(quicksum(X[c, p] for c in CGT[s]) <= len(RGT[s])) for s in S for p in P}
-    c = {c : Quality.addConstr(quicksum(X[c, p] for p in P) == L[c]) for c in Courses}
+    Quality.setObjective(quicksum(5 * W[c] for c in Courses) + quicksum(2 * V[cu,p] for cu in Curricula for p in P));
+    #b
+    RoomsAccomodate = {(s, p) : Quality.addConstr(quicksum(X[c, p] for c in CGT[s]) <= len(RGT[s])) for s in S for p in P}
+    #c
+    AllLecPlanned = {c : Quality.addConstr(quicksum(X[c, p] for p in P) == L[c]) for c in Courses}
+    #Must set z to one to be able to plannign a lecture in this class
     d = {(c, d) : Quality.addConstr(quicksum(X[c, p] for p in D[d]) - Z[c, d] >= 0) for c in Courses for d in range(len(D))}
     e = {(c) : Quality.addConstr(quicksum(Z[c, d] for d in range(len(D))) + W[c] >= MND[c]) for c in Courses}
     f = {(cu, p) : Quality.addConstr(quicksum(X[c, p] for c in Curricula[cu]) - Q[cu, p]  == 0) for cu in Curricula for p in P}
-    g = {(cu,p):  Quality.addConstr(-Q[cu, p-1] + Q[cu,p] - Q[cu, p+1] - V[cu, p] <= 0) for (cu, p) in Q if (cu, p-1) in Q and (cu, p+1) in Q}
+    
+    #Compactness
+    g1 = {(cu,p):  Quality.addConstr(-Q[cu, p-1] + Q[cu,p] - Q[cu, p+1] - V[cu, p] <= 0) for (cu, p) in Q if (cu, p-1) in Q and (cu, p+1) in Q}
+    g2 = {(cu,p):  Quality.addConstr(Q[cu,p] - Q[cu, p+1] - V[cu, p] <= 0) for (cu, p) in Q if (cu, p-1)  not in Q}
+    g3= {(cu,p):  Quality.addConstr(-Q[cu, p-1] + Q[cu,p]  - V[cu, p] <= 0) for (cu, p) in Q if (cu, p+1) not in Q}
+    #Teacher can
     h = {(t, p): Quality.addConstr(quicksum(X[c, p] for c in T[t]) <= 1) for t in T for p in P}
 
+    #print(len(f));
     Quality.optimize();
-    
-    return Quality;
+
+    # for d in D:
+    #     for p in d:
+    #         print("Timeslot: ", str(p));
+    #         for c in Courses:
+    #             if (X[c, p].x > 0.9):
+    #                 print("Course", c, "is planned in Timeslot" , p)
+
+    #
     # for a in X:
     #     if (X[a].x > 0.9):
     #         print(a, X[a].x)
     #
     # print("----------")
-    # for c in Courses:
-    #     #if (W[c].x > 0.9):
-    #     print(c, W[c].x)
-    # for a in Z:
-    #     if (Z[a].x > 0.9):
-    #         print(a, Z[a].x)
+    #for c in Courses:
+        #if (W[c].x > 0.9):
+        #print(c, W[c].x)
+    # for c, d in Z:
+    #     #if (Z[c, d].x > 0.9):
+    #     print(sum(Z[c, d].x for d in range(len(D))), MND[c])
 
     # for a in Q:
     #     if (Q[a].x > 0.9):
@@ -109,10 +126,8 @@ def QualityProblem() :
 
     # for a in V:
     #     if (V[a].x > 0.9):
-    #         print(a, V[a].x)
+    #         print(V[a].x)
     
-
-
 def RoomPlanningProblem() :
     # need to implement the linear version of this algorithm maybe in the future
     RPP  = Model()
@@ -120,8 +135,7 @@ def RoomPlanningProblem() :
     RPP.setObjective(quicksum(s * R[s] for s in S))
     SetCovering = {s :  RPP.addConstr(len(P) * quicksum(R[ss] * ss for ss in SGT[s]) >= quicksum(L[c] for c in CGT[s])) for s in S}
     
-    return RPP
-    # RPP.optimize();
+    RPP.optimize();
 
     # for s in S:
     #     #if R[s].x != 0:
@@ -140,44 +154,123 @@ def TeachingPeriodsProblem() :
     #     if T[p].x > 0.9:
     #         print("Allocated: " +  str(p))
 
+def RoomsPlanningvsQuality():
+    RPQ = Model() 
+
+    X = {(c, p) : RPQ.addVar(vtype = GRB.BINARY) for c in Courses for p in P}
+    Z = {(c, d) : RPQ.addVar(vtype = GRB.BINARY) for c in Courses for d in range(len(D))}
+    W = {(c) : RPQ.addVar(vtype = GRB.INTEGER) for c in Courses}
+    Q = {(cu, p) : RPQ.addVar(vtype = GRB.BINARY) for cu in Curricula for p in P}
+    V = {(cu, p) : RPQ.addVar(vtype = GRB.BINARY) for cu in Curricula for p in P}
+    R = {s : RPQ.addVar(vtype= GRB.INTEGER) for s in S}
+    #R_PLUS = {(s) : RPQ.addVar(vtype = GRB.BINARY) for s in S for p in P}
+
+
+
+    fQual = quicksum(5 * W[c] for c in Courses) + quicksum(2 * V[cu,p] for cu in Curricula for p in P)
+    fSeats = quicksum(s * R[s] for s in S)
+
+
+    c4 = {(s, p): RPQ.addConstr(quicksum(X[c, p] for c in CGT[s])  <= quicksum(R[ss] for ss in SGT[s])) for s in S for p in P}
+
+
+    #4e
+    AllLecPlanned = {c : RPQ.addConstr(quicksum(X[c, p] for p in P) == L[c]) for c in Courses}
+    #Must set z to one to be able to plannign a lecture in this class
+    d = {(c, d) : RPQ.addConstr(quicksum(X[c, p] for p in D[d]) - Z[c, d] >= 0) for c in Courses for d in range(len(D))}
+    e = {(c) : RPQ.addConstr(quicksum(Z[c, d] for d in range(len(D))) + W[c] >= MND[c]) for c in Courses}
+    f = {(cu, p) : RPQ.addConstr(quicksum(X[c, p] for c in Curricula[cu]) - Q[cu, p]  == 0) for cu in Curricula for p in P}
+    
+    #Compactness
+    g1 = {(cu,p):  RPQ.addConstr(-Q[cu, p-1] + Q[cu,p] - Q[cu, p+1] - V[cu, p] <= 0) for (cu, p) in Q if (cu, p-1) in Q and (cu, p+1) in Q}
+    g2 = {(cu,p):  RPQ.addConstr(Q[cu,p] - Q[cu, p+1] - V[cu, p] <= 0) for (cu, p) in Q if (cu, p-1)  not in Q}
+    g3= {(cu,p):  RPQ.addConstr(-Q[cu, p-1] + Q[cu,p]  - V[cu, p] <= 0) for (cu, p) in Q if (cu, p+1) not in Q}
+    #Teacher can
+    h = {(t, p): RPQ.addConstr(quicksum(X[c, p] for c in T[t]) <= 1) for t in T for p in P}
+
+    #4f
+    SetCovering = {s :  RPQ.addConstr(len(P) * quicksum(R[ss] * ss for ss in SGT[s]) >= quicksum(L[c] for c in CGT[s])) for s in S}
+
+    RPQ.setObjective(fQual);
+    RPQ.optimize()
 
 def TeachingPeriodsvsQuality():
-    # quicksum(T[p] for p in P)
-    # quicksum(5 * W[c] for c in Courses) + quicksum(2 * V[cu,p] for cu in Curricula for p in P)
-    
-    
-    TPQ = Model();
-    
+    TPQ = Model()
+    TP = {p : TPQ.addVar(vtype = GRB.BINARY) for p in P}
     X = {(c, p) : TPQ.addVar(vtype = GRB.BINARY) for c in Courses for p in P}
-    W = {(c) : TPQ.addVar() for c in Courses}
+    #print(X)
+    #Minimum working days
     Z = {(c, d) : TPQ.addVar(vtype = GRB.BINARY) for c in Courses for d in range(len(D))}
+    W = {(c) : TPQ.addVar(vtype = GRB.INTEGER) for c in Courses}
     Q = {(cu, p) : TPQ.addVar(vtype = GRB.BINARY) for cu in Curricula for p in P}
     V = {(cu, p) : TPQ.addVar(vtype = GRB.BINARY) for cu in Curricula for p in P}
-    R_PLUS = {s : TPQ.addVar(vtype = GRB.BINARY) for s in S}
 
+
+    fTime = quicksum(TP[p] for p in P)
+    fQual = quicksum(5 * W[c] for c in Courses) + quicksum(2 * V[cu,p] for cu in Curricula for p in P)
+
+    #5c
+    c5 = {(c, p): TPQ.addConstr(X[c, p] - TP[p] <= 0) for c in Courses for p in P}
     
-    c4 = {(s, p): TPQ.addConstr(quicksum(X[c, p] for c in CGT[s]) - R_PLUS[s] <= 0) for s in S for p in P}
-    d4 = { (s) : TPQ.addConstr((R_PLUS[s] - quicksum(R_PLUS[ss] for ss in SGT[s])) == 0) for s in S}
-    
-    # 4e
-    c = {c : TPQ.addConstr(quicksum(X[c, p] for p in P) == L[c]) for c in Courses}
+    RoomsAccomodate = {(s, p) : TPQ.addConstr(quicksum(X[c, p] for c in CGT[s]) <= len(RGT[s])) for s in S for p in P}
+    AllLecPlanned = {c : TPQ.addConstr(quicksum(X[c, p] for p in P) == L[c]) for c in Courses}
+    #Must set z to one to be able to plannign a lecture in this class
     d = {(c, d) : TPQ.addConstr(quicksum(X[c, p] for p in D[d]) - Z[c, d] >= 0) for c in Courses for d in range(len(D))}
     e = {(c) : TPQ.addConstr(quicksum(Z[c, d] for d in range(len(D))) + W[c] >= MND[c]) for c in Courses}
     f = {(cu, p) : TPQ.addConstr(quicksum(X[c, p] for c in Curricula[cu]) - Q[cu, p]  == 0) for cu in Curricula for p in P}
-    g = {(cu,p):  TPQ.addConstr(-Q[cu, p-1] + Q[cu,p] - Q[cu, p+1] - V[cu, p] <= 0) for (cu, p) in Q if (cu, p-1) in Q and (cu, p+1) in Q}
+    
+    #Compactness
+    g1 = {(cu,p):  TPQ.addConstr(-Q[cu, p-1] + Q[cu,p] - Q[cu, p+1] - V[cu, p] <= 0) for (cu, p) in Q if (cu, p-1) in Q and (cu, p+1) in Q}
+    g2 = {(cu,p):  TPQ.addConstr(Q[cu,p] - Q[cu, p+1] - V[cu, p] <= 0) for (cu, p) in Q if (cu, p-1)  not in Q}
+    g3= {(cu,p):  TPQ.addConstr(-Q[cu, p-1] + Q[cu,p]  - V[cu, p] <= 0) for (cu, p) in Q if (cu, p+1) not in Q}
+    #Teacher can
     h = {(t, p): TPQ.addConstr(quicksum(X[c, p] for c in T[t]) <= 1) for t in T for p in P}
 
-    TP = {p : TPQ.addVar(vtype = GRB.BINARY) for p in P}
     SetCovering = {s: TPQ.addConstr(len(RGT[s])*quicksum(TP[p] for p in P) >= quicksum(L[c] for c in CGT[s])) for s in S}
     ConsecPeriods = {p: TPQ.addConstr((TP[p] - TP[p-1]) <= 0) for p in P if p != 1}
-
-    f_seats = quicksum(TP[p] for p in P)
-    #f_qual = quicksum(5 * W[c] for c in Courses) + quicksum(2 * V[cu,p] for cu in Curricula for p in P)
     
-    TPQ.setObjective(f_seats)
+    TPQ.setObjective(fQual);
 
-    TPQ.optimize()
+    TPQ.optimize();
 
+def RoomsPlanningvsTeachingPeriods():
+    RPTP = Model ()
+
+    fTime = quicksum(TP[p] for p in P)
+    fSeats = quicksum(s * R[s] for s in S)
+    X = {(c, p) : RPTP.addVar(vtype = GRB.BINARY) for c in Courses for p in P}
+    Z = {(c, d) : RPTP.addVar(vtype = GRB.BINARY) for c in Courses for d in range(len(D))}
+    W = {(c) : RPTP.addVar(vtype = GRB.INTEGER) for c in Courses}
+    Q = {(cu, p) : RPTP.addVar(vtype = GRB.BINARY) for cu in Curricula for p in P}
+    V = {(cu, p) : RPTP.addVar(vtype = GRB.BINARY) for cu in Curricula for p in P}
+    R = {s : RPTP.addVar(vtype= GRB.INTEGER) for s in S}
+    TP = {p : RPTP.addVar(vtype = GRB.BINARY) for p in P}
+
+
+    AllLecPlanned = {c : RPTP.addConstr(quicksum(X[c, p] for p in P) == L[c]) for c in Courses}
+    #Must set z to one to be able to plannign a lecture in this class
+    d = {(c, d) : RPTP.addConstr(quicksum(X[c, p] for p in D[d]) - Z[c, d] >= 0) for c in Courses for d in range(len(D))}
+    e = {(c) : RPTP.addConstr(quicksum(Z[c, d] for d in range(len(D))) + W[c] >= MND[c]) for c in Courses}
+    f = {(cu, p) : RPTP.addConstr(quicksum(X[c, p] for c in Curricula[cu]) - Q[cu, p]  == 0) for cu in Curricula for p in P}
+    
+    #Compactness
+    g1 = {(cu,p):  RPTP.addConstr(-Q[cu, p-1] + Q[cu,p] - Q[cu, p+1] - V[cu, p] <= 0) for (cu, p) in Q if (cu, p-1) in Q and (cu, p+1) in Q}
+    g2 = {(cu,p):  RPTP.addConstr(Q[cu,p] - Q[cu, p+1] - V[cu, p] <= 0) for (cu, p) in Q if (cu, p-1)  not in Q}
+    g3= {(cu,p):  RPTP.addConstr(-Q[cu, p-1] + Q[cu,p]  - V[cu, p] <= 0) for (cu, p) in Q if (cu, p+1) not in Q}
+    #Teacher can
+    h = {(t, p): RPTP.addConstr(quicksum(X[c, p] for c in T[t]) <= 1) for t in T for p in P}
+
+    SetCoveringR = {s :  RPTP.addConstr(len(P) * quicksum(R[ss] * ss for ss in SGT[s]) >= quicksum(L[c] for c in CGT[s])) for s in S}
+
+    SetCoveringTP = {s: RPTP.addConstr(len(RGT[s])*quicksum(TP[p] for p in P) >= quicksum(L[c] for c in CGT[s])) for s in S}
+    ConsecPeriods = {p: RPTP.addConstr((TP[p] - TP[p-1]) <= 0) for p in P if p != 1}
+
+    c4 = {(s, p): RPTP.addConstr(quicksum(X[c, p] for c in CGT[s])  <= quicksum(R[ss] for ss in SGT[s])) for s in S for p in P}
+    c5 = {(c, p): RPTP.addConstr(X[c, p] - TP[p] <= 0) for c in Courses for p in P}
+
+    RPTP.setObjective(fTime)
+
+    RPTP.optimize();
 
 #TODO : Final Multi-Objective Stuff
 # Room planning vs Teaching Periods
@@ -232,4 +325,4 @@ def TeachingPeriodsvsQuality():
 
 
 if __name__ == "__main__":
-    TeachingPeriodsvsQuality()
+    RoomsPlanningvsQuality()
